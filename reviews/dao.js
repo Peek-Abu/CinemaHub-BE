098da9e1c8d6a5f915408ca5cb06bad698fd016f
dbergs72 <dbergs72@gmail.com>
2023-11-29 11:07:43 -0500

    Add support for updating users review field for posting new comment

--- reviews/dao.js
+++ reviews/dao.js
@@ -1,6 +1,21 @@
 import model from "./model.js";
+import * as userDao from "../users/dao.js";
 
-export const createReview = (review) => model.create(review);
+export const createReview = async (review) => {
+    const user = await userDao.findUserByUsername(review.username);
+    console.log("user is " + JSON.stringify(user) + "from review " + JSON.stringify(review));
+    if (user) {
+        await model.create(review);
+        const created_review = await model.findOne(review);
+        if (user.reviews) {
+            user.reviews.push(created_review._id);
+            await userDao.updateUser(user._id, user);
+        } else {
+            user.reviews = [created_review._id];
+        }
+        await userDao.updateUser(user._id, user);
+    }
+}
 export const findAllReviews = () => model.find().populate("movieId").exec();
 export const findReviewById = (reviewId) => model.findOne({ _id: reviewId });
 export const findReviewByMovieId = (movieId) =>
